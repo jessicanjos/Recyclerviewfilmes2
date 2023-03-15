ea3cebd73c844974ba5ee45a25dd2b5aa37c8ca8
jessicanjos <jessicanjos2003@gmail.com>
2023-03-14 21:31:37 -0300

    Adaptador

--- app/src/main/java/br/com/etecia/recyclerviewfilmes2/Filmes.java
+++ app/src/main/java/br/com/etecia/recyclerviewfilmes2/Filmes.java
@@ -1,4 +1,31 @@
 package br.com.etecia.recyclerviewfilmes2;
 
 public class Filmes {
+    private String titulo;
+    private String descricao;
+    private String categoria;
+    private int imagem;
+
+    public Filmes(String titulo, String descricao, String categoria, int imagem){
+        this.titulo = titulo;
+        this.descricao = descricao;
+        this.categoria = categoria;
+        this.imagem = imagem;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public int getImagem() {
+        return imagem;
+    }
 }
