7a7e83454c636b3cac6baacc3bce74d8b737ee8e
jessicanjos <jessicanjos2003@gmail.com>
2023-03-14 22:15:02 -0300

    RecyclerView lista de filmes

--- app/src/main/java/br/com/etecia/recyclerviewfilmes2/MainActivity.java
+++ app/src/main/java/br/com/etecia/recyclerviewfilmes2/MainActivity.java
@@ -3,13 +3,31 @@ package br.com.etecia.recyclerviewfilmes2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    List<Filmes> listaFilmes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Criando a base de dados para carregamento da lista de filmes
+        listaFilmes = new ArrayList<>();
+
+        listaFilmes.add(
+                new Filmes("Interestelar",
+                        "Sei la mano",
+                        "Drama",
+                        R.drawable.inter)
+        );
+
+        //Criando a classe adaptador e passando os parâmetros
+        MyAdapter adapter = new MyAdapter(getApplicationContext(), );
     }
 }
--- app/src/main/java/br/com/etecia/recyclerviewfilmes2/MyAdapter.java
+++ app/src/main/java/br/com/etecia/recyclerviewfilmes2/MyAdapter.java
@@ -1,12 +1,29 @@
 package br.com.etecia.recyclerviewfilmes2;
 
+import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.List;
+
 public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+
+    private Context mContexto;
+    private List<Filmes> lstFilmes;
+
+    //Criando o construtor da classe com parâmetros
+
+
+    public MyAdapter(Context mContexto, List<Filmes> lstFilmes) {
+        this.mContexto = mContexto;
+        this.lstFilmes = lstFilmes;
+    }
 
     @NonNull
     @Override
@@ -17,6 +34,7 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
 
+
     }
 
     @Override
@@ -24,10 +42,16 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
         return 0;
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder{
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView Titulo;
+        ImageView Imagem;
+        CardView Card;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+            Titulo = itemView.findViewById(R.id.idTituloFilme);
+            Imagem = itemView.findViewById(R.id.idImgFilme);
+            Card = itemView.findViewById(R.id.idCardFilmes);
         }
     }
 
